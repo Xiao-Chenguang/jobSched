f6bf92da64d2a1fd31cf9aa76f6ab5c413647f32
yingshizhang <973990275@qq.com>
2023-04-02 14:55:40 +0100

    move setup to father folder

--- setup.py
+++
@@ -1,8 +0,0 @@
-# import setup module from setuptools
-from setuptools import setup
-
-# call setup function to setup the project
-setup(name='jobSched',
-        version='0.1',
-        description='Job Scheduler',
-)
