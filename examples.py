ca2d61d5bfd45da1eb55d0c8a696cc2ee41fd628
Xiao-Chenguang <973990275@qq.com>
2023-08-25 16:57:29 +0100

    add a python example

--- examples.py
+++ examples.py
@@ -1,1 +1,18 @@
 # examples of jobSched usage
+from jobSched import getJobs
+
+
+def experimet(param1, param2, param3):
+    print("param1: %s, param2: %s, param3: %s" % (param1, param2, param3))
+
+
+if __name__ == '__main__':
+    
+    # prepare parameter lists
+    p1 = [1, 2, 3]
+    p2 = ['a', 'b', 'c']
+    p3 = ['Jeff', 'John', 'Jack']
+
+    # run jobs in parallel
+    for job in getJobs(paramLists=[p1, p2, p3], gpus=4, ppg=True):
+        experimet(*job)
