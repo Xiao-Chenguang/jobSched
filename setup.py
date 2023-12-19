180dc80c0c3b871d334a72d4d5b039784501e2fe
yingshizhang <973990275@qq.com>
2023-12-19 10:25:31 +0000

    add setup as package

---
+++ setup.py
@@ -0,0 +1,8 @@
+# import setup module from setuptools
+from setuptools import setup
+
+# call setup function to setup the project
+setup(name='jobSched',
+        version='0.1',
+        description='Job Scheduler',
+        )
