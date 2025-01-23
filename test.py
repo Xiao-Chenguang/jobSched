ce94853e9a83c1bd153069cbf50cb84454eacccd
Xiao-Chenguang <973990275@qq.com>
2025-01-23 16:44:46 +0000

    fix outdated test case

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from jobSched import getJobs
 
 a = range(3)
-b = ['a', 'b', 'c']
-c = ['x', 'y']
+b = ["a", "b", "c"]
+c = ["x", "y"]
 
-if __name__ == '__main__':
-    for id in range(3):
-        print('='*20, 'Test Case: ', id, '='*20)
-        jobs = getJobs(paramLists=[a, b, c], ppg=3, jobId=id)
+if __name__ == "__main__":
+    for pid in range(3):
+        print("=" * 20, "Test Case: ", pid, "=" * 20)
+        jobs = getJobs(params=[a, b, c], num_task=3, pid=pid)
         # print(jobs)
